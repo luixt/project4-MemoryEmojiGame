0a3ea575b580038a8bc11a70f44c7faaefbdccd8
Luis <luisgabrielda@gmail.com>
2026-03-28 23:20:48 -0400

    Initial Commit

---
+++ project4-MemoryEmojiGame/project4_MemoryEmojiGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  project4_MemoryEmojiGameApp.swift
+//  project4-MemoryEmojiGame
+//
+//  Created by Luis Delgado on 3/28/26.
+//
+
+import SwiftUI
+
+@main
+struct project4_MemoryEmojiGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
